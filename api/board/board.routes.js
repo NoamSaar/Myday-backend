abf95059e336960dc92ffba887d3df1336d6f0e5
Noam Saar <noamsaar11@gmail.com>
2024-01-11 13:11:39 +0200

    Refactor board.routes

---
+++ api/board/board.routes.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { requireAuth } from '../../middlewares/requireAuth.middleware.js'
+import { log } from '../../middlewares/logger.middleware.js'
+import { getBoards, getBoardById, addBoard, updateBoard, removeBoard } from './board.controller.js'
+
+const router = express.Router()
+
+// We can add a middleware for the entire router:
+// router.use(requireAuth)
+
+router.get('/', log, getBoards)
+router.get('/:id', getBoardById)
+router.post('/', addBoard)
+router.put('/', updateBoard)
+router.delete('/:id', removeBoard)
+
+//tasks
+// router.post('/:id/task', addBoardTask)
+// router.delete('/:id/task/:taskId', removeBoardTask)
+
+export const boardRoutes = router
