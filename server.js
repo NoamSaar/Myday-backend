541b32befdcbc6ef5442fd3c6de4417d76df7111
EdenRize <edenrize@gmail.com>
2024-01-16 01:17:33 +0200

    build

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const corsOptions = {
         'http://localhost:8080',
         'http://127.0.0.1:5173',
         'http://localhost:5173',
+        'http://127.0.0.1:5174',
+        'http://localhost:5174',
     ],
     credentials: true
 }
