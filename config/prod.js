1ffb02b0773581dd6e6c1831d4912915ca09fec2
Mor Marzan <mormarzan@gmail.com>
2024-04-09 13:50:35 +0300

    Switch to Mor's db

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://noamsaar:Barbur9726@cluster0.zkdl310.mongodb.net/',
+  // dbURL: process.env.MONGO_URL || 'mongodb+srv://noamsaar:Barbur9726@cluster0.zkdl310.mongodb.net/',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://morMar:RLGW.bf2xq7iCvJ@cluster0.2f9tsun.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   dbName: process.env.DB_NAME || 'myday_db'
 }
